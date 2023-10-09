2d622694ad549cb4de7769fec64961427c73d61b
Delano Francis <deltel000@gmail.com>
2023-10-09 11:24:06 -0500

    Add task 5

---
+++ main.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    option = '0'
+
+    while option != '6':
+        print('1.\tCreate a new disturbance\n2.\tRun One Tick\n3.\tShow All Disturbances\n4.\tShow All Cities\n5.\tDisplay Bulletins\n6.\tExit') 
+        option = input('Select an option from the menu: ')
