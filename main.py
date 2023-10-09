f7f5f00fc87bf83ab616370802b2d1d0ac5b1538
Delano Francis <deltel000@gmail.com>
2023-10-09 11:47:53 -0500

    Add options 1,3,6 for task 6

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from disturbance import makeDisturbance, sayDisturbance
+
 if __name__ == '__main__':
     option = '0'
+    activeDisturbances = []
 
     while option != '6':
         print('1.\tCreate a new disturbance\n2.\tRun One Tick\n3.\tShow All Disturbances\n4.\tShow All Cities\n5.\tDisplay Bulletins\n6.\tExit') 
-        option = input('Select an option from the menu: ')
+        option = input('\n\n\nSelect an option from the menu: ')
+
+        if option == '1':
+            disturbanceName = input('Enter the name of the next disturbance:\n')
+            nextDisturbance = makeDisturbance(disturbanceName)
+            activeDisturbances.append(nextDisturbance)
+        elif option == '3':
+            if len(activeDisturbances) == 0:
+                print('No active disturbances\n\n\n')
+            else:
+                print('The active disturbances are as follows:\n')
+                for disturbance in activeDisturbances:
+                    print(sayDisturbance(disturbance))
+                    print('----------------------------------')
+                print('\n\n\n')
